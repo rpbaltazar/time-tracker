0a16fb84c772d82647924afaf43f64a7da826af6
Rui Baltazar <rui.p.baltazar@gmail.com>
2015-07-01 19:41:50 +0800

    tracking changes on windows focus in infinte loop. To fix

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,5 +8,15 @@ class AppDelegate
     @item = statusBar.statusItemWithLength -1
     @item.setImage timeTrackerImage
     @item.setMenu @mainMenu
+
+    lastActiveApp = nil
+    while true do
+      currentActiveApp = NSWorkspace.sharedWorkspace().activeApplication()
+      if lastActiveApp.nil? || currentActiveApp['NSApplicationName'] != lastActiveApp
+        lastActiveApp = currentActiveApp['NSApplicationName']
+        puts "#{Time.new}: #{lastActiveApp}"
+        sleep 1
+      end
+    end
   end
 end
